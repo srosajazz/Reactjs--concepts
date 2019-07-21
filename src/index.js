d34cb7d3365a37dd55f34ae5c33e4d0a5faecf2e
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-21 10:35:05 -0400

    Creating Root Component src/App.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const sum = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
 
-alert(sum(1, 5));
+import App from './App';
+
+render(<App />, document.getElementById('app'));
