175b87f9451f138fd06fcf4409bab7dbfc2601c6
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-21 12:08:17 -0400

    Handling State in React Immutable Approaches

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: ['Node.JS', 'ReactJS', 'React Native'],
   };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: '',
+    });
+  };
+
   render() {
-    console.log(this.state);
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Send</button>
+      </form>
     );
   }
 }
